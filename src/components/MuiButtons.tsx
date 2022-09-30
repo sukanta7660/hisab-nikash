66f10edfa767bdcafe015a2cf71a911cf2010b2d
Sukanta Purkayastha <30552975+sukanta7660@users.noreply.github.com>
2022-09-30 21:59:38 +0600

    Update MuiButtons.tsx

--- src/components/MuiButtons.tsx
+++ src/components/MuiButtons.tsx
@@ -12,12 +12,36 @@ const MuiButtons = () => {
       </Stack>
 
       <Stack spacing={2} margin={2} direction='row'>
-        <Button variant='contained' color='primary'>Primary</Button>
-        <Button variant='contained' color='secondary'>Secondary</Button>
-        <Button variant='contained' color='error'>Error</Button>
-        <Button variant='contained' color='warning'>Warning</Button>
-        <Button variant='contained' color='info'>Info</Button>
-        <Button variant='contained' color='success'>Success</Button>
+        <Button
+            variant='contained'
+            color='primary'>
+            Primary
+        </Button>
+        <Button
+            variant='contained'
+            color='secondary'>
+            Secondary
+        </Button>
+        <Button
+            variant='contained'
+            color='error'>
+            Error
+        </Button>
+        <Button
+            variant='contained'
+            color='warning'>
+            Warning
+        </Button>
+        <Button
+            variant='contained'
+            color='info'>
+            Info
+        </Button>
+        <Button
+            variant='contained'
+            color='success'>
+            Success
+        </Button>
       </Stack>
 
       <Stack spacing={2} margin={2} direction='row'>
