d74bbba4555cea176e3efa8ccda82c70c59e49d6
Sukanta Purkayastha <30552975+sukanta7660@users.noreply.github.com>
2022-10-25 20:03:29 +0600

    list type defined

--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -1,11 +1,22 @@
 import {Stack, Autocomplete, TextField} from '@mui/material';
 import {useState} from 'react';
 
+type Skill = {
+    id: number,
+    label: string
+}
+
 const skills = ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'React'];
 
+const skillOptions = skills.map((skill, index) => ({
+    id: index + 1,
+    label: skill
+}));
+
 const AutoComplete = () => {
-    const [value, setValue] = useState<string | null>(null)
-    console.log({ value })
+    const [value, setValue] = useState<string | null>(null);
+    const [skill, setSkill] = useState<Skill | null>(null);
+    console.log({ skill })
     return (
         <>
             <Stack spacing={2} width='250px'>
@@ -14,6 +25,14 @@ const AutoComplete = () => {
                     renderInput={(params) => <TextField {...params} label='Skills'/>}
                     value={value}
                     onChange={(event: any, newValue: string | null) => setValue(newValue)}
+                    freeSolo
+                />
+
+                <Autocomplete
+                    options={skillOptions}
+                    renderInput={(params) => <TextField {...params} label='Skills'/>}
+                    value={skill}
+                    onChange={(event: any, newValue: Skill | null) => setSkill(newValue)}
                 />
             </Stack>
         </>
