19a9da44382789357c69e0a3410721c85d3ace6d
Sukanta Purkayastha <30552975+sukanta7660@users.noreply.github.com>
2022-10-25 19:46:06 +0600

    Autocomplete react typescript type defined

--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -1,14 +1,20 @@
 import {Stack, Autocomplete, TextField} from '@mui/material';
+import {useState} from 'react';
 
 const skills = ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'React'];
 
 const AutoComplete = () => {
+    const [value, setValue] = useState<string | null>(null)
+    console.log({ value })
     return (
         <>
             <Stack spacing={2} width='250px'>
                 <Autocomplete
                     options={skills}
-                    renderInput={(params) => <TextField {...params} label='Skills'/>} />
+                    renderInput={(params) => <TextField {...params} label='Skills'/>}
+                    value={value}
+                    onChange={(event: any, newValue: string | null) => setValue(newValue)}
+                />
             </Stack>
         </>
     );
